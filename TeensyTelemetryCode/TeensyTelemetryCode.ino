0b6762bb8daf4fe5077eb3e6afb8d9cfcad7d0b6
Johanna Nguyen <144396118+johannanguyen1@users.noreply.github.com>
2025-04-03 01:59:48 -0400

    edited to include gps, need to calibrate bounds with gps, untested

--- TeensyTelemetryCode/TeensyTelemetryCode.ino
+++ TeensyTelemetryCode/TeensyTelemetryCode.ino
@@ -20,13 +20,14 @@ MCP_CAN CAN(CS_Pin);
 unsigned long lastRPMUpdate = 0;
 unsigned long lastCoolantUpdate = 0;
 unsigned long lastBatteryUpdate = 0;
+unsigned long lastGearUpdate = 0;
 const unsigned long rpmInterval = 500;
 const unsigned long coolantInterval = 2000;
 const unsigned long batteryInterval = 5000;
+const unsigned long gearInterval = 500;
 
 unsigned int rpm, rpm1, rpm2, rpm3dig, gear, coolInTemp, coolOutTemp, batteryVoltage, fuelUsed;
 bool overheating;
-
 
 void setup() {
   pinMode(CS_Pin, OUTPUT);
@@ -59,6 +60,11 @@ void loop() {
     sendBatteryFuel();
     lastBatteryUpdate = currentMillis;
   }
+  if (currentMillis - lastGearUpdate >= gearInterval) {
+    gear = readGearSensor();
+    sendGear();
+    lastGearUpdate = currentMillis;
+  }
 }
 
 void canISR() {
@@ -89,7 +95,6 @@ void processCANMessages() {
 void handleCANMessage(CANMessage msg) {
   if (msg.id == 0x102) {
     rpm = extractFloatFromBuffer(msg.buf) / 6;
-    gear = msg.buf[7];
   } else if (msg.id == 0x103) {
     coolInTemp = extractFloatFromBuffer(msg.buf);
     coolOutTemp = extractFloatFromBuffer(msg.buf + 4);
@@ -104,6 +109,7 @@ void handleCANMessage(CANMessage msg) {
   //}
 }
 
+
 float extractFloatFromBuffer(unsigned char* buf) {
   union {
     uint32_t bits;
@@ -115,28 +121,22 @@ float extractFloatFromBuffer(unsigned char* buf) {
 
 //--------------------SENDING VALUES TO NEXTION---------------------------------------------
 void sendRPM() {
-  rpm3dig = rpm/100; // change RPM from 13500 to 135
-  // NOTE: If out of bounds, meaning a value is sent less than 0 or greater than 100, nextion will recieve the val as 0 for the progress bar in question
-  if (rpm3dig <= 100) { // GREEN: 0-100 RPM, Out of Bound ==> when less than 0, just sends 0 for both progress bars
-    rpm1 = rpm3dig; 
-    rpm2 = 0; 
-  } else if (135 >= rpm3dig || rpm3dig > 100) { // RED(Shift): 101-135 RPM
+  rpm3dig = rpm / 100;
+  if (rpm3dig <= 100) {
+    rpm1 = rpm3dig;
+    rpm2 = 0;
+  } else if (rpm3dig <= 135) {
     rpm1 = 100;
     rpm2 = (rpm3dig % 100) * 100 / 35;
-      // Math explained: 
-      // (rpm3dig % 100) = 135 - 100 = 1 through 35
-      // (y/35) = (x/100)
-      // y = 135 - 100   and   x = ratio from y out of 35, to out of 100 for the progress bar
-      // 100(y/35) = x = rpm2
-  } else if (rpm3dig > 135) { // Out of Bound ==> when RPM > 135, maxes out both progress bars
+  } else {
     rpm1 = 100;
-    rpm2 = 100; 
+    rpm2 = 100;
   }
   sendToNextion("rpm", rpm, true);
   sendToNextion("rpm1", rpm1, true);
   sendToNextion("rpm2", rpm2, true);
-  sendToNextion("gear", gear, true);
 }
+
 void sendCoolantTemp() {
   sendToNextion("coolInTemp", coolInTemp, true);
   sendToNextion("coolOutTemp", coolOutTemp, true);
@@ -144,18 +144,42 @@ void sendCoolantTemp() {
     sendToNextion("overheating", "!!!!!!!!Overheating!!!!!!!!", false);
   }
 }
+
 void sendBatteryFuel() {
   sendToNextion("batteryVoltage", batteryVoltage, true);
   sendToNextion("fuelUsed", fuelUsed, true);
 }
 
+
+// need to calibrate gps first and change bounds for each gear
+int readGearSensor() {
+  int total = 0;
+  for (int i = 0; i < 10; i++) {
+    total += analogRead(A9);
+    delay(10);
+  }
+  int gearSensor = total / 10;
+  if (gearSensor < 170) return 1;
+  else if (gearSensor < 340) return 2;
+  else if (gearSensor < 510) return 3;
+  else if (gearSensor < 680) return 4;
+  else if (gearSensor < 850) return 5;
+  else if (gearSensor <= 1023) return 6;
+  return 0;
+}
+
+void sendGear() {
+  sendToNextion("gear", gear, true);
+}
+
 //--------------------SOMETHING FAILED, DISPLAY ERROR MESSAGE ON DASH--------------------------------------
-void sendErrorCase(const String& message) { // message must be 30 characters or LESS
+// message must be 30 characters or LESS
+void sendErrorCase(const String& message) {
   sendToNextion("overheating", message, false);
 }
 
 void sendToNextion(const String& objectName, const String& value, bool isNumeric) {
-  Serial1.print(objectName + (isNumeric ? ".val=" : ".txt=\"") + value + (isNumeric ? "" : "\""));
+  Serial1.print(objectName + (isNumeric ? ".val=" : ".txt=\"") + value + (isNumeric ? "" : "\"""));
   Serial1.write(0xFF);
   Serial1.write(0xFF);
   Serial1.write(0xFF);
