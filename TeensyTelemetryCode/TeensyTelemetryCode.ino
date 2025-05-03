3c90925d17596371ac33fd228a50a1f9a6366ae5
Min04C <123710130+Min04C@users.noreply.github.com>
2025-05-03 16:39:57 -0400

    Change Pages function (analog pin A9)

--- TeensyTelemetryCode/TeensyTelemetryCode.ino
+++ TeensyTelemetryCode/TeensyTelemetryCode.ino
@@ -1,17 +1,8 @@
-// ISSUES with current code
-// 1) rpm worked fine before the function for the sendErrorCase() was added but is now unreliable, why? (in the meantime, commented out)
-// 2) dash turns off while car starts the motor. assume the car pulls all power for this and since the dash is on the same 
-//    power source, it loses power for a second, then turns back on (this has been an issue from the start, working on an 
-//    isolated power supply)
-// 4) sometimes car will start, dash will update but then freeze, why? (not really an issue anymore)
-
 #include <mcp_can.h>  // necessary for CAN Protocol communication commands
 #include <SPI.h>
 
 #define CS_Pin 10
 #define INTRPT_Pin 9  // Interrupt pin
-//#define Page_Pin 23  // assign any analog pin on the teensy
-
 
 struct CANMessage {
   unsigned long id;
@@ -35,14 +26,16 @@ const unsigned long coolantInterval = 2000;
 const unsigned long batteryInterval = 5000;
 const unsigned long gearInterval = 500;
 
-// unsigned int current_page = 0;
 unsigned int rpm, rpm1, rpm2, rpm3dig, gear, coolInTemp, coolOutTemp, batteryVoltage, fuelUsed;
 bool overheating;
+
+// Page Values
+const int page_Pin = A9; // Analog pin for Page Dial
+unsigned int last_page = -1;
 
 void setup() {
   pinMode(CS_Pin, OUTPUT);
   pinMode(INTRPT_Pin, INPUT);
-  //pinMode(Page_Pin, INPUT); // for changing the page
   Serial1.begin(9600);
 
   if (CAN.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ) == CAN_OK) {
@@ -56,17 +49,17 @@ void setup() {
 }
 
 void loop() {
-  // add code here to read analoug pin for changing pages
-  /* Something like:
-    pageVal = analogRead(Page_Pin);
-    if (pageVal != current_page) {
-      current_page = analogRead(Page_Pin);
-      Serial1.print("page" + String(current_page)); Serial1.write(0xFF); Serial1.write(0xFF); Serial1.write(0xFF);
-    }
-  */
+  // Change Pages w/ dial
+  float pageVolt = analogRead(Page_Pin) * (3.3 / 1023.0); // from 0 to 1023 --> from 0.0 to 3.3
+  int currentPage = (pageVolt < 1.65) ? 0 : 1; // page0: <1.65  page2>=1.65
+  if (currentPage != last_page) { 
+    Serial1.print("page" + String(currentPafe)); Serial1.write(0xFF); Serial1.write(0xFF); Serial1.write(0xFF);
+    lastPage = currentPage;
+  }
+
   processCANMessages();
   unsigned long currentMillis = millis();
-  if (currentMillis - lastRPMUpdate >= rpmInterval) {
+  if (currentMillis - lastRPMUpdate >= rpmInterval) { //RPM
     sendRPM();
     lastRPMUpdate = currentMillis;
   }
@@ -92,12 +85,8 @@ void canISR() {
     if (nextHead != bufferTail) {
       canBuffer[bufferHead] = msg;
       bufferHead = nextHead;
-    } //else {
-      //sendErrorCase("#1 Buffer overflow"); //does this NEED to be an error message?
-    //}
-  } //else {
-    //sendErrorCase("#2 no CAN message"); //does this NEED to be an error message?
-  //}
+    } 
+  }
 }
 
 //-------------------SET VARIABLES FROM CAN PACKET-------------------------------------------
@@ -122,11 +111,8 @@ void handleCANMessage(CANMessage msg) {
   } else if (msg.id == 0x104) {
     batteryVoltage = extractFloatFromBuffer(msg.buf) * 100;
     fuelUsed = extractFloatFromBuffer(msg.buf + 4) * 100;
-  } //else { //double check if this even needs to be an error case, or simply ignored
-    //sendErrorCase("#4 ids not found"); // message ids not found
-  //}
+  } 
 }
-
 
 float extractFloatFromBuffer(unsigned char* buf) {
   union {
@@ -168,36 +154,9 @@ void sendBatteryFuel() {
   sendToNextion("fuelUsed", fuelUsed, true);
 }
 
-/* don't need this since gear position sensor passes through the motec
-// need to calibrate gps first and change bounds for each gear
-int readGearSensor() {
-  int total = 0;
-  for (int i = 0; i < 10; i++) {
-    total += analogRead(A9);
-    delay(10);
-  }
-  int gearSensor = total / 10;
-  if (gearSensor < 170) return 1;
-  else if (gearSensor < 340) return 2;
-  else if (gearSensor < 510) return 3;
-  else if (gearSensor < 680) return 4;
-  else if (gearSensor < 850) return 5;
-  else if (gearSensor <= 1023) return 6;
-  return 0;
-}
-*/
-
 void sendGear() {
   sendToNextion("gear", gear, true);
 }
-
-
-//--------------------SOMETHING FAILED, DISPLAY ERROR MESSAGE ON DASH--------------------------------------
-// message must be 30 characters or LESS
-// this is for debugging purposes, but lowkey might be making things worse...
-//void sendErrorCase(const String& message) {
-//  sendToNextion("overheating", message, false);
-//}
 
 void sendToNextion(const String& objectName, const String& value, bool isNumeric) {
   Serial1.print(objectName + (isNumeric ? ".val=" : ".txt=\"") + value + (isNumeric ? "" : "\""));
